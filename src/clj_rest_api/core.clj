680eeb7c01abd1fac5a3a7e83ef62b2a2d7612de
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-12-29 19:48:08 +0900

    Add root path

--- src/clj_rest_api/core.clj
+++ src/clj_rest_api/core.clj
@@ -1,7 +1,33 @@
 (ns clj-rest-api.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [integrant.core :as ig]
+   [ring.adapter.jetty :as jetty]
+   [ring.util.response :as response]))
+
+;;; handlers
+
+(defn hello-world [_request]
+  (response/response "Hello, World!"))
+
+(defmethod ig/init-key ::app [_ _]
+  hello-world)
+
+;;; API server
+
+(defmethod ig/init-key ::server [_ {:keys [app options]}]
+  (jetty/run-jetty app options))
+
+(defmethod ig/halt-key! ::server [_ server]
+  (.stop server))
+
+(def config
+  {::app {}
+   ::server {:app (ig/ref ::app)
+             :options {:port 3000
+                       :join? false}}})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (ig/init config))
